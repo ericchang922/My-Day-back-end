4059fdf88202702e3c2a64a9996c8853fccf39f5
ChangCheWei <10656004@ntub.edu.tw>
2021-03-14 21:47:31 +0800

    Create project

---
+++ studyplan/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudyplanConfig(AppConfig):
+    name = 'studyplan'
---
+++ temporary_group/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TemporaryGroupConfig(AppConfig):
+    name = 'temporary_group'
