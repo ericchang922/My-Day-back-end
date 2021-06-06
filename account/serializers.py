2a4e682ae2eddbd4ff8f4344d105b93f05503b31
BotoWu <10656003@ntub.edu.tw>
2021-06-06 18:01:22 +0800

    Create register

---
+++ account/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from api.models import Account
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['name', 'user_id', 'password']
+
+    def save(self):
+        account = Account(
+            user_id=self.validated_data['user_id'],
+            name=self.validated_data['name'],
+            password=self.validated_data['password'],
+        )
+        account.save()
+        return account
+
+
+class changepwSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['user_id', 'password']
--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from api.models import Account
+from account.serializers import RegisterSerializer
+
 
 # Create your views here.
+class AccountViewSet(ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = RegisterSerializer
+
+    @action(detail=False, methods=['POST'])
+    def register(self, request):
+
+        serializer = RegisterSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['uid'] = account.user_id
+            data['userName'] = account.name
+            data['password'] = account.password
+            data['response'] = '成功'
+
+        return Response(data)
+
+
