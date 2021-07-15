329e1c15e79e8e7453215a048ee392ef9efc36f0
Hsin Yi Lin <10656025@ntub.edu.tw>
2021-07-16 01:11:00 +0800

    Perf temporary_group/views.py
    優化temporary_group程式

--- temporary_group/views.py
+++ temporary_group/views.py
@@ -6,7 +6,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
 from temporary_group.serializers import ScheduleSerializer
-from api.models import Schedule, Group, TemporaryList, GetGroup, GetTemporaryInvite, GetGroupNoVote, GroupMember
+from api.models import Schedule, Group, TemporaryList, GetGroup, GetTemporaryInvite, GetGroupNoVote, GroupMember, \
+    GroupLog
 
 
 # Create your views here.
@@ -19,23 +20,28 @@ class TemporaryGroupViewSet(ModelViewSet):
         data = request.data
 
         uid = data.get('uid')
-        group_no = data.get('group_no')
+        groupNum = data.get('groupNum')
         title = data.get('title')
         startTime = data.get('startTime')
         endTime = data.get('endTime')
         typeId = data.get('typeId')
 
-        group_no = Group.objects.get(pk=group_no)
-
-        Schedule.objects.create(connect_group_no=group_no,schedule_name=title,
-                                schedule_start=startTime,schedule_end=endTime,
-                                type_id=typeId)
-
+        group_member = GroupMember.objects.filter(group_no=groupNum, user_id=uid, status_id__in=[1, 4])
+        if group_member.exists():
+            groupNum = Group.objects.get(serial_no=groupNum)
+            Schedule.objects.create(connect_group_no=groupNum,schedule_name=title,
+                                    schedule_start=startTime,schedule_end=endTime,
+                                    type_id=typeId)
+            GroupLog.objects.create(do_time=datetime.now(), group_no=groupNum,user_id=uid,
+                                    trigger_type='I', do_type_id=3)
+            return Response({
+                'response': True,
+                'message': '成功'
+            })
         return Response({
-            'response': True,
-            'message': '成功'
+            'response': False,
+            'message': '非群組成員，故無法建立'
         })
-
 
     @action(detail=False)
     def temporary_list(self, request):
@@ -45,53 +51,41 @@ class TemporaryGroupViewSet(ModelViewSet):
         tg = TemporaryList.objects.filter(user_id=uid,status_id__in=[1,4],
                                           is_temporary_group=1, endtime__gte=datetime.now())
 
-        if tg.count() > 0:
-            return Response({
-                'temporaryContent': [
-                    {
-                        'groupId': g.group_no,
-                        'typeId': g.type_id,
-                        'title': g.group_name,
-                        'startTime': g.starttime,
-                        'endTime': g.endtime,
-                        'peopleCount': g.cnt,
-                    }
-                    for g in tg
-                ]
-            })
-        else:
-            return Response({
-                'response': False,
-                'message': '沒有玩聚'
-            })
+        return Response({
+            'temporaryContent': [
+                {
+                    'groupId': g.group_no,
+                    'typeId': g.type_id,
+                    'title': g.group_name,
+                    'startTime': g.starttime,
+                    'endTime': g.endtime,
+                    'peopleCount': g.cnt,
+                }
+                for g in tg
+            ]
+        })
 
     @action(detail=False)
     def invite_list(self, request):
         data = request.query_params
 
         uid = data.get('uid')
-        tg = TemporaryList.objects.filter(user_id=uid, status_id=2,
-                                          is_temporary_group=1, endtime__gte=datetime.now())
+        tmp_group = TemporaryList.objects.filter(user_id=uid, status_id=2,
+                                                is_temporary_group=1, endtime__gte=datetime.now())
 
-        if tg.count() > 0:
-            return Response({
-                'temporaryContent': [
-                    {
-                        'groupId': g.group_no,
-                        'typeId': g.type_id,
-                        'title': g.group_name,
-                        'startTime': g.starttime,
-                        'endTime': g.endtime,
-                        'peopleCount': g.cnt,
-                    }
-                    for g in tg
-                ]
-            })
-        else:
-            return Response({
-                'response': False,
-                'message': '沒有玩聚邀請'
-            })
+        return Response({
+            'temporaryContent': [
+                {
+                    'groupId': g.group_no,
+                    'typeId': g.type_id,
+                    'title': g.group_name,
+                    'startTime': g.starttime,
+                    'endTime': g.endtime,
+                    'peopleCount': g.cnt,
+                }
+                for g in tmp_group
+            ]
+        })
 
     @action(detail=False)
     def get(self, request):
@@ -100,55 +94,52 @@ class TemporaryGroupViewSet(ModelViewSet):
         uid = data.get('uid')
         groupNum = data.get('groupNum')
 
-        gr = GroupMember.objects.filter(user_id=uid, group_no=groupNum, status_id__in=[1, 4])
-        if gr.count() > 0:
-            tg = GetGroup.objects.filter(user_id=uid, group_no=groupNum, status_id__in=[1, 4])
-            if tg.count() > 0:
-                return Response({
-                    'title': tg.first().group_name,
-                    'typeId': tg.first().type_id,
-                    'vote': [
-                        {
-                            'title': g.title,
-                            'voteNum': g.votenum,
-                            'isVoteType': g.votetype,
-                        }
-                        for g in tg
-                    ]
-                })
-            else:
-                tg = GetGroupNoVote.objects.filter(user_id=uid, group_no=groupNum, status_id__in=[1, 4])
-                return Response({
-                    'title': tg.first().group_name,
-                    'typeId': tg.first().type_id,
-                })
-        else:
+        group = GroupMember.objects.filter(user_id=uid, group_no=groupNum, status_id__in=[1, 4])
+        get_group = GetGroup.objects.filter(user_id=uid, group_no=groupNum, status_id__in=[1, 4])
+        group_has_vote = 1 if get_group.count() > 0 else 0
+
+        if group.count() == 0:
             return Response({
                 'response': False,
                 'message': '您不屬於此群組'
             })
+        elif get_group.count() == 0:
+            get_group = GetGroupNoVote.objects.filter(user_id=uid, group_no=groupNum, status_id__in=[1, 4])
+
+        return Response({
+            'title': get_group.first().group_name,
+            'typeId': get_group.first().type_id,
+            'vote': [
+                {
+                    'title': g.title,
+                    'voteNum': g.votenum,
+                    'isVoteType': g.votetype,
+                }
+                for g in get_group if group_has_vote
+            ]
+        })
 
     @action(detail=False)
     def get_invite(self, request):
         data = request.query_params
 
         groupNum = data.get('groupNum')
-        tg = GetTemporaryInvite.objects.filter(group_no=groupNum)
+        tmp_group_invite = GetTemporaryInvite.objects.filter(group_no=groupNum)
 
-        if tg.count() > 0:
+        if tmp_group_invite.count() > 0:
             return Response({
-                'title': tg.first().group_name,
-                'startTime': tg.first().found_time,
-                'endTime': tg.first().schedule_end,
-                'founderPhoto': base64.b64encode(tg.first().founder_photo),
-                'founderName': tg.first().founder_name,
+                'title': tmp_group_invite.first().group_name,
+                'startTime': tmp_group_invite.first().found_time,
+                'endTime': tmp_group_invite.first().schedule_end,
+                'founderPhoto': base64.b64encode(tmp_group_invite.first().founder_photo),
+                'founderName': tmp_group_invite.first().founder_name,
                 'member': [
                     {
                         'memberPhoto': base64.b64encode(g.member_photo),
                         'memberName': g.member_name,
                         'statusId': g.status_id,
                     }
-                    for g in tg
+                    for g in tmp_group_invite
                 ]
             })
         else:
