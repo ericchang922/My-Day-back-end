f3a53750427234c7162664cecf4b3524536f457b
BotoWu <10656003@ntub.edu.tw>
2021-07-15 15:56:31 +0800

    Add account(change_pw and login)

--- account/views.py
+++ account/views.py
@@ -5,13 +5,13 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.models import Account
-from account.serializers import RegisterSerializer
+from account.serializers import AccountSerializer
 
 
 # Create your views here.
 class AccountViewSet(ModelViewSet):
     queryset = Account.objects.all()
-    serializer_class = RegisterSerializer
+    serializer_class = AccountSerializer
 
     @action(detail=False, methods=['POST'])
     def register(self, request):
@@ -32,3 +32,36 @@ class AccountViewSet(ModelViewSet):
         except:
             return Response({'response': False, 'message': '錯誤'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'Response': True, 'message': '成功'}, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['POST'])
+    def change_pw(self, request):
+        data = request.data
+
+        uid = data.get('uid')
+        password = data.get('password')
+
+        try:
+            account = Account.objects.get(user_id=uid)
+        except:
+            return Response({'request': False, 'message': '沒有此帳號'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if password is not None:
+            account.password = password
+
+        account.save()
+
+        return Response({'response': True, 'message': '更改密碼成功'}, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['POST'])
+    def login(self, request):
+        data = request.data
+
+        uid = data.get('uid')
+        password = data.get('password')
+
+        try:
+            account = Account.objects.get(user_id=uid, password=password)
+        except:
+            return Response({'request': False, 'message': '登入失敗'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'response': True, 'message': account.name}, status=status.HTTP_200_OK)
