f887e2b68cf9dec7f92b5a3c2aad9e85c9e3f643
BotoWu <10656003@ntub.edu.tw>
2021-08-12 15:46:58 +0800

    Add timetable(edit_timetable, delete_timetable)

--- timetable/views.py
+++ timetable/views.py
@@ -84,3 +84,138 @@ class TimetableViewSet(ModelViewSet):
                 return err(Msg.Err.Timetable.create, 'TI-A-007', request)
 
         return success(request=request)
+
+    @action(detail=False, methods=['POST'])
+    def edit_timetable(self, request):
+        data = request.data
+
+        uid = data['uid']
+        school_year = data['schoolYear']
+        semester = data['semester']
+        f_semester = f'{school_year}-{semester}'
+        subject = data['subject']
+
+        try:
+            p_timetable = PersonalTimetable.objects.filter(user_id=uid, semester=f_semester)
+        except:
+            return err(Msg.Err.Timetable.get_timetable, 'TI-B-001', request)
+        timetable_no = p_timetable[0].timetable_no.serial_no
+        school_no = p_timetable[0].school_no
+
+        try:
+            timetable = Timetable.objects.filter(timetable_no=timetable_no)
+        except:
+            return err()
+
+        timetable_list = []
+        for t in timetable:
+            timetable_list.append(int(t.section_no.section_no))
+
+        edit_list = []
+
+        for i in subject:
+            subject_name = i.get('subjectName')
+            start_time = i.get('startTime')
+            end_time = i.get('endTime')
+            section = i.get('section')
+            week = i.get('week')
+            section_no = week_name[week] + section
+
+            if week and section is not None:
+                try:
+                    section_db = Section.objects.get(weekday=week, section=section)
+                except ObjectDoesNotExist:
+                    section_db = Section.objects.create(section_no=section_no, weekday=week, section=section)
+                except:
+                    return err(Msg.Err.Timetable.section_create, 'TI-B-002', request)
+
+            if subject_name is not None:
+                try:
+                    subject_db = Subject.objects.get(subject_name=subject_name)
+                except ObjectDoesNotExist:
+                    subject_db = Subject.objects.create(subject_name=subject_name)
+                except:
+                    return err(Msg.Err.Timetable.subject_create, 'TI-B-003', request)
+
+                if section_no in timetable_list:
+                    # try:
+                    timetable_update = Timetable.objects.filter(timetable_no_id=timetable_no, section_no=section_db)
+                    timetable_update.update(subject_no=subject_db)
+                    # except:
+                    #     return err(Msg.Err.Timetable.update, 'TI-B-004', request)
+                else:
+                    try:
+                        Timetable.objects.create(timetable_no_id=timetable_no, section_no=section_db,
+                                                 subject_no=subject_db)
+                    except:
+                        return err(Msg.Err.Timetable.create, 'TI-B-005', request)
+
+                # try:
+                get_classtime = ClassTime.objects.filter(school_no=school_no, section_no=section_db)
+
+                if start_time is not None:
+                    get_classtime.update(start=start_time)
+                if end_time is not None:
+                    get_classtime.update(end=end_time)
+                # except:
+                # return err(Msg.Err.Timetable.section_update, 'TI-B-006', request)
+
+                if get_classtime.count() <= 0:
+                    ClassTime.objects.create(school_no=school_no, section_no=section_db, start=start_time, end=end_time)
+                edit_list.append(section_no)
+
+        for t in timetable_list:
+            if t not in edit_list:
+                Timetable.objects.get(timetable_no=timetable_no, section_no=t).delete()
+
+        return success()
+
+    @action(detail=False, methods=['POST'])
+    def delete_timetable(self, request):
+        data = request.data
+
+        uid = data.get('uid')
+        school_year = data.get('schoolYear')
+        semester = data.get('semester')
+        f_semester = f'{school_year}-{semester}'
+
+        try:
+            p_timetable = PersonalTimetable.objects.filter(user_id=uid, semester=f_semester)
+        except:
+            return err(Msg.Err.Timetable.get_timetable, 'TI-C-001', request)
+
+        try:
+            p_school = School.objects.get(serial_no=p_timetable[0].school_no.serial_no)
+        except ObjectDoesNotExist:
+            return not_found(Msg.NotFound.school, request)
+        except:
+            return err(Msg.Err.Timetable.get_school, 'TI-C-002', request)
+
+        try:
+            p_class = ClassTime.objects.filter(school_no=p_school)
+        except ObjectDoesNotExist:
+            return not_found(Msg.NotFound.class_time, request)
+        except:
+            return err(Msg.Err.Timetable.get_classtime, 'TI-C-002', request)
+
+        try:
+            timetable = Timetable.objects.filter(timetalbe_no=p_timetable[0].timetable_no.serial_no)
+        except ObjectDoesNotExist:
+            return not_found(Msg.NotFound.timetable, request)
+        except:
+            return err(Msg.Err.get_time, 'TI-C-004', request)
+
+        try:
+            create = TimetableCreate.objects.get(serial_no=p_timetable[0].timetable_no.serial_no)
+        except ObjectDoesNotExist:
+            return not_found(Msg.NotFound.timetable, request)
+        except:
+            return err(Msg.Err.get_timetable, 'TI-C-005', request)
+
+        p_timetable.delete()
+        p_class.delete()
+        p_school.delete()
+        timetable.delete()
+        create.delete()
+
+        return success()
